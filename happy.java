0cec5b3d38cbb15043c6fe23ddb60482aa2cd2de
Benji2008 <hiltibic4869@student.faytechcc.edu>
2026-02-05 18:59:19 +0000

    worlds simplist game

--- happy.java
+++ happy.java
@@ -1,6 +1,12 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class happy {
+    public static int roll() {
+        Random roller = new Random();
+        int roll = roller.nextInt(5) + 1;
+        return roll;
+    }
     public static void main(String[] args){
         System.out.println("hello whats your name");
         Scanner input = new Scanner(System.in); 
@@ -8,7 +14,16 @@ public class happy {
 
         System.out.println();
         System.out.println("nice to meetcha, " + name);
+        int chips = 100;
+        System.out.println("well," + name + " you start with " + chips + " chips. ");
+      
+      for (int i=0; i < 20; i++ ) {
+            System.out.println ("rolling two dice...🎲 ");
+             int roll = roll() + roll();
+             System.out.println(" you rolled a : " + roll);
+        }
+        input.close();
 
 
-}
+    }
 }
